b65989c31730b8e965c12554f92e246d8937a221
ensta <ensta@LAPTOP-56R9U5BD.wifi.uc.edu>
2024-03-28 09:20:57 -0400

    First Commit

--- Team1_Assignment09/mainPackage/main.py
+++ Team1_Assignment09/mainPackage/main.py
@@ -1,1 +1,18 @@
 # main.py
+
+import requests
+
+url = "https://tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com/getMLBTeamRoster"
+
+querystring = {"teamAbv":"CHW","getStats":"true"}
+
+headers = {
+    "X-RapidAPI-Key": "894022e47emsh03029ed244790f4p1aba49jsn06684cbe4ee2",
+    "X-RapidAPI-Host": "tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
+
